35f0b6f3007490b6ff3a8f2b5ee1084461e6e2a8
zhuosi <wangzhuosi13@hotmail.com>
2016-04-20 02:10:16 -0700

    created the yogasettingActivity.java

---
+++ BeeCalm/mobile/src/main/java/wecare/beecalm/YogaSettingActivity.java
@@ -0,0 +1,20 @@
+package wecare.beecalm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+/**
+ * Created by Zhuosi on 4/20/16.
+ */
+public class YogaSettingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoga_setting);
+        setTitle("Yoga Settings");
+
+    }
+
+}
