4cb292401a3c81ddc685f3ceee1e6fe0232d2882
zhuosi <wangzhuosi13@hotmail.com>
2016-04-20 20:09:06 -0700

    added all pages and images that are needed.

---
+++ BeeCalm/wear/src/main/java/wecare/beecalm/BreathActivity.java
@@ -0,0 +1,19 @@
+package wecare.beecalm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+/**
+ * Created by Zhuosi on 4/20/16.
+ */
+
+public class BreathActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breath);
+
+    }
+}
--- BeeCalm/wear/src/main/java/wecare/beecalm/WelcomeActivity.java
+++ BeeCalm/wear/src/main/java/wecare/beecalm/WelcomeActivity.java
@@ -1,17 +1,28 @@
 package wecare.beecalm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class WelcomeActivity extends Activity {
 
-    private TextView mTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new CountDownTimer(2000, 1000) {
+            public void onFinish() {
+                Intent startActivity = new Intent(WelcomeActivity.this,BreathActivity.class);
+                startActivity(startActivity);
+                finish();
+            }
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+        }.start();
     }
 }
